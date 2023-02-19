2cfd79d0974f4785c1858d68be9a324ebeec3ff3
Idam <i.faizin@cebes.co.id>
2023-02-19 22:04:24 +0700

    add scss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1 class="text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="main">
+    <h1 class="text-red-500">Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style lang="scss">
+    .main {
+        p {
+            @apply text-blue-500;
+        }
+    }
+</style>
