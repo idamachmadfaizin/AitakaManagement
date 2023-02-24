796f72577c7fd62bb4e152691c543192ca604cd5
Idam <34857067+idamachmadfaizin@users.noreply.github.com>
2023-02-24 23:39:32 +0700

    MySidebar component use dynamic component

--- src/components/MySidebar.svelte
+++ src/components/MySidebar.svelte
@@ -8,82 +8,72 @@
 		SidebarItem,
 		SidebarWrapper
 	} from 'flowbite-svelte';
+	import { children } from 'svelte/internal';
+	import ChartPieIcon from './icons/heroicons/ChartPieIcon.svelte';
+	import Squares2x2Icon from './icons/heroicons/Squares2x2Icon.svelte';
+
+	interface ISidebarItem {
+		label: string;
+		activeUrl: string;
+		iconComponent?: any;
+		children?: Omit<ISidebarItem, 'children' | 'iconComponent'>[];
+	}
 
 	$: activeUrl = $page.url.pathname;
 
+	const iconClass = 'w-6 h-6';
 	let spanClass = 'flex-1 ml-3 whitespace-nowrap';
 
+	let sideBarItems: ISidebarItem[] = [
+		{
+			label: 'Dashboard',
+			activeUrl: '/',
+			iconComponent: ChartPieIcon
+		},
+		{
+			label: 'Product',
+			activeUrl: '/product',
+			iconComponent: Squares2x2Icon,
+			children: [
+				{
+					label: 'Add Product',
+					activeUrl: '/add-product'
+				},
+				{
+					label: 'Product List',
+					activeUrl: '/product-list'
+				}
+			]
+		},
+		{
+			label: 'Order',
+			activeUrl: '/order',
+			iconComponent: ChartPieIcon
+		}
+	];
 </script>
 
 <Sidebar class={$$props.class}>
 	<SidebarWrapper class="h-full rounded-none">
 		<SidebarGroup>
-			<SidebarItem label="Dashboard" active={activeUrl === '/'}>
-				<svelte:fragment slot="icon">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
-						/>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
-						/>
-					</svg>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarDropdownWrapper label="Product">
-				<svelte:fragment slot="icon">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
-						/>
-					</svg>
-				</svelte:fragment>
-				<SidebarDropdownItem label="Add Product" />
-				<SidebarDropdownItem label="Product List" />
-			</SidebarDropdownWrapper>
-			<SidebarItem label="Order" active={activeUrl === '/order'}>
-				<svelte:fragment slot="icon">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
-						/>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
-						/>
-					</svg>
-				</svelte:fragment>
-			</SidebarItem>
+			{#each sideBarItems as sideBar (sideBar.activeUrl)}
+				{#if sideBar.children && sideBar.children.length > 0}
+					<SidebarDropdownWrapper label={sideBar.label}>
+						<svelte:fragment slot="icon">
+							<svelte:component this={sideBar.iconComponent} class={iconClass} />
+						</svelte:fragment>
+						{#each sideBar.children as child (child.activeUrl)}
+							<SidebarDropdownItem label={child.label} active={activeUrl === child.activeUrl} />
+						{/each}
+					</SidebarDropdownWrapper>
+				{:else}
+					<SidebarItem label={sideBar.label} active={activeUrl === sideBar.activeUrl}>
+						<svelte:fragment slot="icon">
+							<svelte:component this={sideBar.iconComponent} class={iconClass} />
+						</svelte:fragment>
+					</SidebarItem>
+				{/if}
+			{/each}
 		</SidebarGroup>
 	</SidebarWrapper>
 </Sidebar>
