21519a889eed9f2ed174edc04485325ff3911d78
Idam <i.faizin@cebes.co.id>
2023-02-24 21:19:45 +0700

    create sidebar component

---
+++ src/components/MySidebar.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import {
+		Sidebar,
+		SidebarDropdownItem,
+		SidebarDropdownWrapper,
+		SidebarGroup,
+		SidebarItem,
+		SidebarWrapper
+	} from 'flowbite-svelte';
+
+	$: activeUrl = $page.url.pathname;
+
+	let spanClass = 'flex-1 ml-3 whitespace-nowrap';
+
+</script>
+
+<Sidebar class={$$props.class}>
+	<SidebarWrapper class="h-full rounded-none">
+		<SidebarGroup>
+			<SidebarItem label="Dashboard" active={activeUrl === '/'}>
+				<svelte:fragment slot="icon">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
+						/>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
+						/>
+					</svg>
+				</svelte:fragment>
+			</SidebarItem>
+			<SidebarDropdownWrapper label="Product">
+				<svelte:fragment slot="icon">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+						/>
+					</svg>
+				</svelte:fragment>
+				<SidebarDropdownItem label="Add Product" />
+				<SidebarDropdownItem label="Product List" />
+			</SidebarDropdownWrapper>
+			<SidebarItem label="Order" active={activeUrl === '/order'}>
+				<svelte:fragment slot="icon">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
+						/>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
+						/>
+					</svg>
+				</svelte:fragment>
+			</SidebarItem>
+		</SidebarGroup>
+	</SidebarWrapper>
+</Sidebar>
