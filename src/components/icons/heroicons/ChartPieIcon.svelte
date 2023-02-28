91d2c029b8ce909d02e056353c34bdcaa05e093a
Idam <34857067+idamachmadfaizin@users.noreply.github.com>
2023-03-01 00:46:34 +0700

    update heroicons

--- src/components/icons/heroicons/ChartPieIcon.svelte
+++ src/components/icons/heroicons/ChartPieIcon.svelte
@@ -1,4 +1,11 @@
-<svg class={$$props.class} fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+<script lang="ts">
+  export let fill = "none";
+  export let stroke = "currentColor";
+  export let strokeWidth = 1.5;
+  export let viewBox = "0 0 24 24";
+</script>
+
+<svg class={$$props.class} {fill} {stroke} stroke-width={strokeWidth} {viewBox} xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
   <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"></path>
   <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"></path>
 </svg>
---
+++ src/components/icons/heroicons/CubeIcon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let fill = "none";
+    export let stroke = "currentColor";
+    export let strokeWidth = 1.5;
+    export let viewBox = "0 0 24 24";
+</script>
+
+<svg class={$$props.class} {fill} {stroke} stroke-width={strokeWidth} {viewBox} xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"></path>
+</svg>
